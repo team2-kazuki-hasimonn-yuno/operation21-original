84a18c0792021188e963603b53c15a353c88b3ce
mikachi20 <mKs1m400H@gmail.com>
2022-06-03 10:21:47 +0900

    apikeyを隠すようにした

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
+import SampleFirebase from "../views/sample_firebase.vue"
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/samplefirebase",
+    name: "SampleFirebase",
+    component: SampleFirebase,
   },
   {
     path: "/about",
---
+++ src/views/sample_firebase.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <button v-on:click="postTweet">ツイート</button>
+  </div>
+</template>
+
+<script>
+import { collection, addDoc } from "firebase/firestore"
+// firebase.js で db として export したものを import
+import { db } from "../firebase"
+
+export default {
+  methods: {
+    postTweet() {
+      console.log("発火")
+      addDoc(collection(db, "sample"), {
+        name: "test",
+      })
+    },
+  },
+}
+</script>
